697302abaac6e2e990f397efddecdfdc8fbeb519
Verbum-Daemonis <amormara.fati@gmail.com>
2026-07-27 14:14:22 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE='lunaris-verbum-v4';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('message',e=>{if(e.data?.type==='SHOW_NOTIFICATION')e.waitUntil(self.registration.showNotification(e.data.title,{body:e.data.body,icon:'./assets/icon-192.png',badge:'./assets/icon-192.png',tag:e.data.tag,renotify:true,data:{url:'./#planner'}}))});
-self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.openWindow('./#planner'))});
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return res}).catch(()=>caches.match('./index.html')))));
